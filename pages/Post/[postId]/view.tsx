827389292d207a31dc2c48e7d6d57938146af064
rafi1919 <rafifourze1904@gmail.com>
2024-05-10 18:54:51 +0700

    fix eslint

--- pages/Post/[postId]/view.tsx
+++ pages/Post/[postId]/view.tsx
@@ -1,54 +1,60 @@
-import Layout from "@/templates/components/Layout"
-import {FaUser} from 'react-icons/fa'
+import Layout from "@/templates/components/Layout";
+import { FaUser } from "react-icons/fa";
 
-interface detailProps{
-    postId?:any
-    commentsData:any
-    userName:any
-    body: string
-    title: string
+interface detailProps {
+  postId?: any;
+  commentsData: any;
+  userName: any;
+  body: string;
+  title: string;
 }
 
-const PostDetailView:React.FC<detailProps>=({ commentsData, title, body, userName})=>{
-    return(
-        <Layout  heading={title}>
-                <div className="w-full">
-                    {/* <p className="text-xl font-bold py-2">{title}</p> */}
-                    <p className="text-left">by: 
+const PostDetailView: React.FC<detailProps> = ({
+  commentsData,
+  title,
+  body,
+  userName,
+}) => {
+  return (
+    <Layout heading={title}>
+      <div className="w-full">
+        {/* <p className="text-xl font-bold py-2">{title}</p> */}
+        <p className="text-left">
+          by:
+          <span className="text-green-600 font-medium">{userName}</span>
+        </p>
+        <p className="text-[18px] py-5">{body}</p>
+      </div>
+      <div className="flex flex-col w-full">
+        <p className="text-md font-bold ">Comments:</p>
+        <div className="py-3">
+          <ul className="flex flex-col gap-4">
+            {(commentsData?.length ?? 0) > 0 ? (
+              <>
+                {commentsData.map((item: any, index: any) => (
+                  <div className="" key={index}>
+                    <div className=" flex items-center">
+                      <div className="rounded-l-md p-3 border-[1px]  text-black bg-green-500">
+                        <FaUser />
+                      </div>
+                      <p className="bg-green-300 p-2 rounded-r-md text-white w-fit">
+                        {item.name}
+                      </p>
+                    </div>
+                    <p className="px-2 text-xl font-semibold">{item.body}</p>
+                  </div>
+                ))}
+              </>
+            ) : (
+              <>
+                <p className="font-bold text-xl">No Comments</p>
+              </>
+            )}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-                    <span className="text-green-600 font-medium">{userName}</span>
-                    </p>
-                    <p className="text-[18px] py-5">{body}</p>
-                
-                </div>
-            <div className="flex flex-col w-full">
-                <p className="text-md font-bold ">Comments:</p>
-                <div className="py-3">
-                    <ul className="flex flex-col gap-4">
-                        {(commentsData?.length ?? 0)> 0 ? <>
-                            
-                            {commentsData.map((item:any)=>(
-                                <div className="">
-                                    <div className=" flex items-center">
-                                        <div className="rounded-l-md p-3 border-[1px]  text-black bg-green-500">
-                                            <FaUser />
-                                        </div>
-                                        <p className="bg-green-300 p-2 rounded-r-md text-white w-fit">{item.name}</p>
-                                    </div>
-                                    <p className="px-2 text-xl font-semibold">{item.body}</p>
-                                </div>
-                            ))}
-                        
-                        </>: <>
-                            <p className="font-bold text-xl">No Comments</p>
-                        </>}
-                       
-                    </ul>
-                </div>
-            </div>
-            
-        </Layout>
-    )
-}
-
-export default PostDetailView
+export default PostDetailView;
--- pages/User/[userId]/index.tsx
+++ pages/User/[userId]/index.tsx
@@ -1,37 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import UserDetailView from './view';
-import { useRouter } from 'next/router';
-import { useGetUserDetail } from '@/hooks';
+import React, { useState, useEffect } from "react";
+import UserDetailView from "./view";
+import { useRouter } from "next/router";
+import { useGetUserDetail } from "@/hooks";
 
 export default function Index() {
-    const router = useRouter();
-    const userId = router.query.userId as string;
-    const [userData, setUserData] = useState<any>({});
-    const { data, loading, getUserDetail } = useGetUserDetail();
+  const router = useRouter();
+  const userId = router.query.userId as string;
+  const [userData, setUserData] = useState<any>({});
+  const { data, loading, getUserDetail } = useGetUserDetail();
 
-    useEffect(() => {
-        if (userId) {
-            getUserDetail(userId);
-        }
-    }, [userId]);
-
-    useEffect(() => {
-        if (data && Object.keys(data).length > 0) { 
-            setUserData(data);
-        }
-    }, [data]);
-
-    if (loading) {
-        return <div>Loading...</div>;
+  useEffect(() => {
+    if (userId) {
+      getUserDetail(userId);
     }
+  }, [userId, getUserDetail]);
 
-    return (
-        <UserDetailView
-            name={userData.name}
-            email={userData.email}
-            gender={userData.gender}
-            status={userData.status}
-            userId={userId}
-        />
-    );
+  useEffect(() => {
+    if (data && Object.keys(data).length > 0) {
+      setUserData(data);
+    }
+  }, [data]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <UserDetailView
+      name={userData.name}
+      email={userData.email}
+      gender={userData.gender}
+      status={userData.status}
+      userId={userId}
+    />
+  );
 }
